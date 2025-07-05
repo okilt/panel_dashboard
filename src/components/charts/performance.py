f8574f20b6d078f2b038738e8dbed31299f00210
okilt <swqllowok@gmail.com>
2025-07-06 00:59:42 +0900

    Updated pyproject to allow unused import for hvplot.pandas

--- src/components/charts/performance.py
+++ src/components/charts/performance.py
@@ -1,25 +1,30 @@
-import panel as pn
 import hvplot.pandas
-from src.state import state
+import panel as pn
+
 from src.models.factory import get_performance_data
+from src.state import state
 
 pn.extension()
+
 
 class PerformanceChart(pn.viewable.Viewer):
     def __init__(self, **params):
         super().__init__(**params)
         # This component's view is bound directly to the global state's parameter
-        self._view = pn.panel(pn.bind(self._get_plot, fund_id=state.param.selected_fund_id))
+        self._view = pn.panel(
+            pn.bind(self._get_plot, fund_id=state.param.selected_fund_id)
+        )
 
     async def _get_plot(self, fund_id):
-        yield pn.indicators.LoadingSpinner(value=True, width=50, height=50, align='center')
-        
+        yield pn.indicators.LoadingSpinner(
+            value=True, width=50, height=50, align="center"
+        )
+
         perf_df = await get_performance_data(fund_id)
-        
+
         plot = perf_df.hvplot.line(
-            title=f"Performance for {fund_id}",
-            height=350, responsive=True, grid=True
-        ).opts(yformatter='%.2f')
+            title=f"Performance for {fund_id}", height=350, responsive=True, grid=True
+        ).opts(yformatter="%.2f")
         yield plot
 
     def __panel__(self):
